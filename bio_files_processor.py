b158934b02a69c878fb8574a04d22ebc3c1fbabd
Anastasia <AnaSPK@yandex.ru>
2025-10-13 00:56:38 +0500

    add comments in code

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -4,20 +4,20 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta = 'output_fasta
         for line in fasta_file:
             if line.startswith(">"):
                 if sequence:
-                        processed_fasta.write(f'{sequence}\n')
+                        processed_fasta.write(f'{sequence}\n')  # write the sequence in one line to output file
                         sequence = ''
-                processed_fasta.write(line)
+                processed_fasta.write(line)  # write the secuence id to output file
             else:
                 sequence += line.strip()
         if sequence:
-            processed_fasta.write(sequence.strip())
+            processed_fasta.write(sequence.strip())  # write the last sequence
 
 
 def parse_blast_output(input_file, output_file = 'best_proteins'):
     with open(input_file) as blast_results, open(output_file, 'a') as best_proteins:
         query = False
         for line in blast_results:
-            if query:
+            if query:  # process the next line after "Description"
                 if "]" not in line:
                     line = line.replace("...", "]")
                     gene = line.split(']')[0]
